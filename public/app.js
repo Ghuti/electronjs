40beb0c5d7628c0d1c5787cfed51eed1a657ebd8
Ghutilife <yannick.remy16@gmail.com>
2019-12-04 16:50:03 +0100

    last step

--- public/app.js
+++ public/app.js
@@ -1,10 +1,10 @@
-const buttons = document.querySelectorAll('.btn');
+/*const buttons = document.querySelectorAll('.btn');
 const result = document.getElementById('result');
 
 console.log(buttons);
 
 buttons.forEach(element => {
-  element.addEventListener('click', function() {
+  element.addEventListener('click' , function() {
     let value = this.textContent;
 
     if (value == 'C') {
@@ -13,13 +13,83 @@ buttons.forEach(element => {
     else if (value == '<==') {
       result.innerHTML = result.innerHTML.slice(0, -1);
     }
+    else if (value == '=') {
+      result.innerHTML += '';
+      parseInt
+    }
     else {
-    result.innerHTML += value;
-    console.log(value);
+      result.innerHTML += value;
+      console.log(value);
     }
 
     if (result.textContent === '') {
       result.innerHTML = 0;
+    }
+  })
+})
+*/
+
+function isSymbol(value) {
+  return ['/', '*', '-', '+', '%','.'].indexOf(value) !== -1
+}
+
+function getLastChar() {
+  return result.innerHTML.substr(-1)
+
+}
+
+function addToresult(value) {
+  if (!isStarted) {
+    if (!isSymbol(value)) {
+      result.innerHTML = null
+    }
+    isStarted = true
+  }
+  if (isSymbol(getLastChar()) && isSymbol(value)) {
+    removeresult()
+  }
+  result.innerHTML += value
+}
+
+function removeresult(length = -1) {
+  result.innerHTML = result.innerHTML.slice(0, length)
+  if (!result.innerHTML) {
+    result.innerHTML = 0
+    isStarted = false
+  }
+}
+
+function refreshresult() {
+  result.innerHTML = 0
+  isStarted = false
+}
+
+fonction calcu() {
+  resultF = eval(result.innerHTML);
+  result.innerHTML = resultF;
+}
+
+const result = document.getElementById('result')
+const buttons = document.querySelectorAll('.btn')
+
+let isStarted = false
+
+buttons.forEach(element => {
+  element.addEventListener('click', function() {
+
+    let value = this.textContent
+    switch (value) {
+      case 'C':
+        refreshresult();
+        break;
+      case '<==':
+        removeresult();
+        break;
+      case '='
+        calcu();
+        break;
+      default:
+        addToresult(value);
     }
   })
 })
