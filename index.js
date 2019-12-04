f695520a6f3bc3d7bfd8b943fb0010cade7f9996
Ghutilife <yannick.remy16@gmail.com>
2019-12-04 11:17:25 +0100

    first step

---
+++ index.js
@@ -0,0 +1,36 @@
+const {app, BrowserWindow} = require('electron');
+
+
+let win
+
+function createWindow() {
+  win = new BrowserWindow({
+    width: 800,
+    heignt: 600,
+    resizable: false
+  })
+  win.loadFile('public/index.html')
+
+  //OUVRE LES DEVTOOLS
+  win.webContents.openDevTools()
+
+  win.on('closed', () => {
+    win = null
+  })
+}
+
+app.on('ready', function() {
+  createWindow()
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (win === null) {
+    createWindow()
+  }
+})
